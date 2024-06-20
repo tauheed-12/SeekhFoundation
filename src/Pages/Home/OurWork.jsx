f77c4ff46ea911ea2f25c53ef45c2927f18ce893
tauheed-12 <mohammadtauheed160@gmail.com>
2024-06-20 15:46:51 +0530

    sixth commit

--- src/Pages/Home/OurWork.jsx
+++ src/Pages/Home/OurWork.jsx
@@ -6,7 +6,6 @@ import news4 from "../../assets/img4.jpg";
 import news5 from "../../assets/img8.jpg";
 import news6 from "../../assets/img10.jpg";
 
-
 const OurWork = () => {
     return (
         <div className="p-4">
@@ -14,40 +13,46 @@ const OurWork = () => {
                 <h1 className="text-3xl font-bold text-center">Our <span className="text-greenColor">Works</span></h1>
             </div>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-                <div className="relative overflow-hidden rounded-lg">
+                <div className="relative overflow-hidden rounded-lg group">
                     <img src={news1} alt="News 1" className="w-full h-48 object-cover" />
-                    <div className="absolute bottom-0 left-0 bg-gray-800 bg-opacity-50 text-white p-2 w-full">
-                        <p>News Description 1</p>
+                    <h1 className="absolute bottom-0 left-0 bg-green-400 bg-opacity-50 text-white p-2 w-full opacity-100 group-hover:opacity-0 transition-opacity duration-300">News 1 title</h1>
+                    <div className="absolute bottom-0 left-0 bg-green-600 bg-opacity-50 text-white p-2 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                 </div>
-                <div className="relative overflow-hidden rounded-lg">
+                <div className="relative overflow-hidden rounded-lg group">
                     <img src={news2} alt="News 2" className="w-full h-64 object-cover" />
-                    <div className="absolute bottom-0 left-0 bg-gray-800 bg-opacity-50 text-white p-2 w-full">
-                        <p>News Description 2</p>
+                    <h1 className="absolute bottom-0 left-0 bg-green-400 bg-opacity-50 text-white p-2 w-full opacity-100 group-hover:opacity-0 transition-opacity duration-300">News 1 title</h1>
+                    <div className="absolute bottom-0 left-0 bg-green-600 bg-opacity-50 text-white p-2 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                 </div>
-                <div className="relative overflow-hidden rounded-lg">
+                <div className="relative overflow-hidden rounded-lg group">
                     <img src={news3} alt="News 3" className="w-full h-40 object-cover" />
-                    <div className="absolute bottom-0 left-0 bg-gray-800 bg-opacity-50 text-white p-2 w-full">
-                        <p>News Description 3</p>
+                    <h1 className="absolute bottom-0 left-0 bg-green-400 bg-opacity-50 text-white p-2 w-full opacity-100 group-hover:opacity-0 transition-opacity duration-300">News 1 title</h1>
+                    <div className="absolute bottom-0 left-0 bg-green-600 bg-opacity-50 text-white p-2 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                 </div>
-                <div className="relative overflow-hidden rounded-lg">
+                <div className="relative overflow-hidden rounded-lg group">
                     <img src={news4} alt="News 4" className="w-full h-56 object-cover" />
-                    <div className="absolute bottom-0 left-0 bg-gray-800 bg-opacity-50 text-white p-2 w-full">
-                        <p>News Description 4</p>
+                    <h1 className="absolute bottom-0 left-0 bg-green-400 bg-opacity-50 text-white p-2 w-full opacity-100 group-hover:opacity-0 transition-opacity duration-300">News 1 title</h1>
+                    <div className="absolute bottom-0 left-0 bg-green-600 bg-opacity-50 text-white p-2 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                 </div>
-                <div className="relative overflow-hidden rounded-lg">
+                <div className="relative overflow-hidden rounded-lg group">
                     <img src={news5} alt="News 5" className="w-full h-72 object-cover" />
-                    <div className="absolute bottom-0 left-0 bg-gray-800 bg-opacity-50 text-white p-2 w-full">
-                        <p>News Description 5</p>
+                    <h1 className="absolute bottom-0 left-0 bg-green-400 bg-opacity-50 text-white p-2 w-full opacity-100 group-hover:opacity-0 transition-opacity duration-300">News 1 title</h1>
+                    <div className="absolute bottom-0 left-0 bg-green-600 bg-opacity-50 text-white p-2 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                 </div>
-                <div className="relative overflow-hidden rounded-lg">
+                <div className="relative overflow-hidden rounded-lg group">
                     <img src={news6} alt="News 6" className="w-full h-60 object-cover" />
-                    <div className="absolute bottom-0 left-0 bg-gray-800 bg-opacity-50 text-white p-2 w-full">
-                        <p>News Description 6</p>
+                    <h1 className="absolute bottom-0 left-0 bg-green-400 bg-opacity-50 text-white p-2 w-full opacity-100 group-hover:opacity-0 transition-opacity duration-300">News 1 title</h1>
+                    <div className="absolute text-sm bottom-0 left-0 bg-green-600 bg-opacity-50 text-white p-2 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </div>
                 </div>
             </div>
